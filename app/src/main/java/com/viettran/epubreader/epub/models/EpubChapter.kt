366e7775b6817169ccb873e7a1e9dd0ca0c46cdc
viettranassaabloy <viet.tran@assaabloy.com>
2026-04-02 01:46:56 +0700

    Add EpubChapter data class

---
+++ app/src/main/java/com/viettran/epubreader/epub/models/EpubChapter.kt
@@ -0,0 +1,9 @@
+package com.viettran.epubreader.epub.models
+
+data class EpubChapter(
+    val chapterId: String,
+    val absPath: String,
+    val title: String,
+    val body: List<ReaderItem> = emptyList(),
+    val nextFragmentId: String? = null
+)
